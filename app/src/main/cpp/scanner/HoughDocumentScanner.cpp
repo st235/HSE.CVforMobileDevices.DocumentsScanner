006c4aaf0dfd1664f1db63e1672242315591cab7
Alexander Dadukin <hi@st235.xyz>
2024-02-06 23:51:52 +0000

    Fixes for hough document scanner

--- app/src/main/cpp/scanner/HoughDocumentScanner.cpp
+++ app/src/main/cpp/scanner/HoughDocumentScanner.cpp
@@ -8,10 +8,40 @@
 
 namespace {
 
+struct Line {
+    double rho;
+    double theta;
+    uint32_t count;
+
+    Line(double rho, double theta, uint32_t count) {
+        this->rho = rho;
+        this->theta = theta;
+        this->count = count;
+    }
+
+    Line(const Line& that) {
+        if (this != &that) {
+            this->rho = that.rho;
+            this->theta = that.theta;
+            this->count = that.count;
+        }
+    }
+
+    Line& operator=(const Line& that) {
+        if (this != &that) {
+            this->rho = that.rho;
+            this->theta = that.theta;
+            this->count = that.count;
+        }
+
+        return *this;
+    }
+};
+
 void HoughTransform(const cv::Mat& image,
                     uint16_t thetaStep,
                     uint16_t pointsPerLineThreshold,
-                    std::vector<std::pair<cv::Point, cv::Point>>& pointsOut) {
+                    std::vector<Line>& pointsOut) {
     // Max distance to the line equals to the length of a diagonal of the image.
     int maxDistance = hypot(image.rows, image.cols);
 
@@ -24,7 +54,7 @@ void HoughTransform(const cv::Mat& image,
                 continue;
             }
 
-            for (uint16_t t = 0; t <= 360; t += thetaStep) {
+            for (uint16_t t = 0; t < 360; t += thetaStep) {
                 // Converting degrees to radians.
                 double theta = 1.0 * t * M_PI / 180.0;
                 auto rho = static_cast<int32_t>(c*std::cos(theta) + r*std::sin(theta));
@@ -40,24 +70,11 @@ void HoughTransform(const cv::Mat& image,
 
     for (size_t theta = 0; theta < houghSpace.rows; theta++) {
         for (size_t rho = 1; rho < houghSpace.cols; rho++) {
-            if (houghSpace.at<uint16_t>(theta, rho) < pointsPerLineThreshold) {
+            const auto count = houghSpace.at<uint16_t>(theta, rho);
+            if (count < pointsPerLineThreshold) {
                 continue;
             }
-
-            cv::Point p1;
-            cv::Point p2;
-            double thetaRadians = 1.0 * theta * M_PI / 180.0;
-
-            int x0 = cvRound(rho * cos(thetaRadians));
-            int y0 = cvRound(rho * sin(thetaRadians));
-
-            p1.x = cvRound(x0 + 1000 * (-sin(thetaRadians)));
-            p1.y = cvRound(y0 + 1000 * (cos(thetaRadians)));
-
-            p2.x = cvRound(x0 - 1000 * (-sin(thetaRadians)));
-            p2.y = cvRound(y0 - 1000 * (cos(thetaRadians)));
-
-            pointsOut.emplace_back(std::make_pair(p1, p2));
+            pointsOut.emplace_back(Line(rho, theta, count));
         }
     }
 }
@@ -70,14 +87,9 @@ bool HoughDocumentScanner::findCorners(const cv::Mat& image,
                                        float corners[8]) const {
     cv::Mat interimImage;
     cv::cvtColor(image, interimImage, cv::COLOR_BGR2GRAY);
+    cv::Canny(interimImage, interimImage, /* threshold1= */ 50, /* threshold2= */ 200);
 
-    const auto& kernel =
-            cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-    cv::morphologyEx(interimImage, interimImage, cv::MORPH_DILATE, kernel,  /* anchor= */ cv::Point(-1, -1),  /* iterations= */ 5);
-    cv::GaussianBlur(interimImage, interimImage, cv::Size(3, 3), 0);
-    cv::morphologyEx(interimImage, interimImage, cv::MORPH_ERODE, kernel,  /* anchor= */ cv::Point(-1, -1),  /* iterations= */ 5);
 
-    cv::Canny(interimImage, interimImage, /* threshold1= */ 100, /* threshold2= */ 200);
 }
 
 } // namespace scanner
